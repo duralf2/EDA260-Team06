0f24a7256b8042dad31c30a5a494059ed6aa7691
Magnus Tullberg <magnus.tullberg@gmail.com>
2015-01-26 14:04:04 +0100

    Magnus och Kasper. 3.2, fixed method readStartTime and readFinishTime

--- src/io/ReadFile.java
+++ src/io/ReadFile.java
@@ -11,54 +11,66 @@ import java.util.Scanner;
 import com.googlecode.jcsv.reader.CSVReader;
 import com.googlecode.jcsv.reader.internal.CSVReaderBuilder;
 
+import register.model.Contestant;
 import register.model.DataStructure;
 import register.model.Time;
 
 public class ReadFile {
-	StringBuilder sb;
 
 	public ReadFile() {
-		sb = new StringBuilder();
-		// br = new BufferedReader();
 	}
 
-	public boolean read(File file, DataStructure ds) throws IOException {
-//		Reader reader;
+	public boolean readStartTime(File file, DataStructure ds) throws IOException {
 		try {
 			Reader reader = new FileReader(file);
-			CSVReader<String[]> csvParser = CSVReaderBuilder.newDefaultReader(reader);
+			CSVReader<String[]> csvParser = CSVReaderBuilder
+					.newDefaultReader(reader);
 			List<String[]> data = csvParser.readAll();
-			String startNr, startTime, endTime, totalTime;
-			
+			String startNr, time;
+			Contestant contestant;
 			for (String[] line : data) {
 				startNr = line[0];
-				totalTime = line[1];
-				totalTime =totalTime.replaceAll("\\s+","");
-				startTime = line[2];
-				startTime =startTime.replaceAll("\\s+","");
-				endTime = line[3];
-				endTime =endTime.replaceAll("\\s+",""); 
-				ds.addTimeEntry(startNr, new Time(startTime, endTime));
-//				for(String text : line){
-//					sb.append(text);
-//				}
+				time = line[1];
+				time = time.replaceAll("\\s+", "");
+				contestant = ds.getContestant(startNr);
+				if (contestant == null) {
+					contestant = new Contestant("");
+					ds.addContestantEntry(startNr, contestant);
+				}
+				contestant.setStartTime(new Time(time));
 			}
-
 		} catch (IOException e) {
 			throw e;
 		} finally {
-//			try {
-//				r.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
 		}
 		return true;
-
 	}
 
-//	public String toString() {
-//		return sb.toString();
-//	}
+	public boolean readFinishTime(File file, DataStructure ds) throws IOException {
+		try {
+			Reader reader = new FileReader(file);
+			CSVReader<String[]> csvParser = CSVReaderBuilder
+					.newDefaultReader(reader);
+			List<String[]> data = csvParser.readAll();
+			String startNr, time;
+			Contestant contestant;
+			for (String[] line : data) {
+				startNr = line[0];
+				time = line[1];
+				time = time.replaceAll("\\s+", "");
+				contestant = ds.getContestant(startNr);
+				if (contestant == null) {
+					contestant = new Contestant("");
+					ds.addContestantEntry(startNr, contestant);
+				}
+				contestant.setFinishTime(new Time(time));
+			}
+		} catch (IOException e) {
+			throw e;
+		} finally {
+		}
+		return true;
+	}
+
 
 }

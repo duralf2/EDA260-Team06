b6eb66cfdde79baae2a7c23461a7506c9891c54e
zionzept <dat13tha@student.lu.se>
2015-01-26 10:01:08 +0100

    Uppdaterat IOHandler till att hantera tider, samt uppdaterat test. Gör om test för flera fall då vi har en sortmetod implementerad. // Tom&&Oscar

--- src/io/IOHandler.java
+++ src/io/IOHandler.java
@@ -1,20 +1,25 @@
 package io;
 
 import java.io.PrintWriter;
+import java.util.Map;
 
 import register.model.DataStructure;
+import register.model.Time;
 
 public class IOHandler {
 
 	public static void writeResult(PrintWriter pw, DataStructure ds)
 	{
 		StringBuilder sb = new StringBuilder();
+		Map <String, Time> entries = ds.getAllEntries();
 		sb.append("StartNr; TotalTid; Starttid; Måltid\n");
 		// loopa genom ds.driver/time
-		sb.append("1; ");
-		sb.append("--.--.--; ");
-		sb.append("12.00.00; ");
-		sb.append("13.23.34");
+		for (String s: entries.keySet()){
+			sb.append(s + "; ");
+			sb.append("--.--.--; ");
+			sb.append(entries.get(s).getStartTime() + "; ");
+			sb.append(entries.get(s).getFinishTime() + '\n');
+		}
 		pw.write(sb.toString());
 		pw.close();
 	}
@@ -22,14 +27,26 @@ public class IOHandler {
 	public static void writeStartTimes(PrintWriter pw, DataStructure ds)
 	{
 		// loopa genom ds.driver/time
-		pw.write("1; 12.00.00");
+		Map <String, Time> entries = ds.getAllEntries();
+		StringBuilder sb = new StringBuilder();
+		for (String s: entries.keySet()){
+			sb.append(s + "; ");
+			sb.append(entries.get(s).getStartTime() );
+		}
+		pw.write(sb.toString());
 		pw.close();
 	}
 	
 	public static void writeGoalTimes(PrintWriter pw, DataStructure ds)
 	{
 		// loopa genom ds.driver/time
-		pw.write("1; 13.23.34");
+		Map <String, Time> entries = ds.getAllEntries();
+		StringBuilder sb = new StringBuilder();
+		for (String s: entries.keySet()){
+			sb.append(s + "; ");
+			sb.append(entries.get(s).getFinishTime() );
+		}
+		pw.write(sb.toString());
 		pw.close();
 	}
 }
--- src/tests/OutputTest.java
+++ src/tests/OutputTest.java
@@ -14,6 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import register.model.DataStructure;
+import register.model.Time;
 
 public class OutputTest {
 
@@ -29,6 +30,7 @@ public class OutputTest {
 		FileOutputStream fos = new FileOutputStream(f);
 		pw = new PrintWriter(fos);
 		ds = new DataStructure();
+		ds.addEntry("1", new Time("12.00.00", "13.23.34"));
 		sc = new Scanner(f);
 	}
 	
@@ -42,27 +44,45 @@ public class OutputTest {
 	public void testWriteResults() {
 		IOHandler.writeResult(pw, ds);
 		assert(f.exists());
-		assertEquals(sc.nextLine(), "StartNr; TotalTid; Starttid; Måltid");
-		assertEquals(sc.next(), "1;");
-		assertEquals(sc.next(), "--.--.--;");
-		assertEquals(sc.next(), "12.00.00;");
-		assertEquals(sc.next(), "13.23.34");
+		assertEquals("StartNr; TotalTid; Starttid; Måltid", sc.nextLine());
+		assertEquals("1;", sc.next());
+		assertEquals("--.--.--;",  sc.next());
+		assertEquals("12.00.00;",  sc.next());
+		assertEquals("13.23.34",  sc.next());
 	}
 
+//	Gör om när vi har en sortmetod!!
+//	@Test
+//	public void testWriteTwoResults() {
+//		ds.addEntry("2", new Time("13.37.00", "13.37.01"));
+//		IOHandler.writeResult(pw, ds);
+//		assert(f.exists());
+//		assertEquals("StartNr; TotalTid; Starttid; Måltid", sc.nextLine());
+//		assertEquals("1;", sc.next());
+//		assertEquals("--.--.--;",  sc.next());
+//		assertEquals("12.00.00;",  sc.next());
+//		assertEquals("13.23.34",  sc.next());
+//		assertEquals("2;", sc.next());
+//		assertEquals("--.--.--;",  sc.next());
+//		assertEquals("13.37.00;",  sc.next());
+//		assertEquals("13.37.01",  sc.next());
+//	}
+	
+	
 	@Test
 	public void testWriteStartTimes() {
 		IOHandler.writeStartTimes(pw, ds);
 		assert(f.exists());
-		assertEquals(sc.next(), "1;");
-		assertEquals(sc.next(), "12.00.00");
+		assertEquals("1;", sc.next());
+		assertEquals("12.00.00",  sc.next());
 	}
 
 	@Test
 	public void testWriteGoalTimes() {
 		IOHandler.writeGoalTimes(pw, ds);
 		assert(f.exists());
-		assertEquals(sc.next(), "1;");
-		assertEquals(sc.next(), "13.23.34");
+		assertEquals("1;", sc.next());
+		assertEquals("13.23.34", sc.next());
 	}
 
 	

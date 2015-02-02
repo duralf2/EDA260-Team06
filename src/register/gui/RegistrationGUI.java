e449be85fc92ef4046e3646c77c3d9e481d8bc9f
Sebastian-0 <dat13shj@student.lu.se>
2015-02-02 09:53:08 +0100

    Refactored RegistrationGUI, Sebastian and Magnus. Story 7

--- src/register/gui/RegistrationGUI.java
+++ src/register/gui/RegistrationGUI.java
@@ -1,28 +1,17 @@
 package register.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
-import javax.swing.JTextField;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.TitledBorder;
 
 public class RegistrationGUI extends JFrame {
@@ -30,8 +19,8 @@ public class RegistrationGUI extends JFrame {
 	private StartNumberField startNumberField;
 
 	
-	public RegistrationGUI(String registrationString) {
-		super(registrationString);
+	public RegistrationGUI(String title) {
+		super(title);
 
 		GridBagLayout gbl = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
@@ -50,15 +39,12 @@ public class RegistrationGUI extends JFrame {
 		titledBorder.setTitleFont(entryList.getFont().deriveFont(Font.BOLD, fontSize/2));
 		entryList.setBorder(titledBorder);
 		
-
-		
 		addToGrid(startNumberLabel, gbl, c, 0, 0, 1, 1, GridBagConstraints.NONE, 0, 0);
 		addToGrid(startNumberField, gbl, c, 1, 0, 1, 1, GridBagConstraints.HORIZONTAL, 1, 0);
 		addToGrid(registerButton  , gbl, c, 2, 0, 1, 1, GridBagConstraints.NONE, 0, 0);
 		addToGrid(entryList	      , gbl, c, 0, 1, 3, 1, GridBagConstraints.BOTH, 1, 1);
 		
 		pack();
-		setMinimumSize(getSize());
 		
 		setExtendedState(MAXIMIZED_BOTH);
 		
@@ -84,11 +70,10 @@ public class RegistrationGUI extends JFrame {
 	
 	private int fontSize() {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(
+		Insets screenInsets = Toolkit.getDefaultToolkit().getScreenInsets(
 				getGraphicsConfiguration());
-		int taskBarSize = scnMax.bottom;
-		int fontSize = ((screenSize.width - getWidth()) + (screenSize.height
-				- taskBarSize - getHeight())) / 60;
+		int taskBarSize = screenInsets.bottom;
+		int fontSize = (screenSize.height - taskBarSize - getHeight()) / 20;
 		return fontSize;
 	}
 
@@ -101,6 +86,6 @@ public class RegistrationGUI extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		RegistrationGUI screen = new RegistrationGUI("Registration window");
+		new RegistrationGUI("Registration window");
 	}
 }

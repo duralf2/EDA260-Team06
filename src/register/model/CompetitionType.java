4fb6f56b7e981bf6efb63bae358eb8394f0a455f
Ella Eriksson <ada10eer@student.lu.se>
2015-02-09 15:16:05 +0100

    Changed interface CompetitionTypes. Story Refactory. Ella Magnus

--- src/register/model/CompetitionType.java
+++ src/register/model/CompetitionType.java
@@ -4,9 +4,7 @@ import java.io.PrintWriter;
 
 public interface CompetitionType {
 	
-	void result(DataStructure ds, PrintWriter pw);
-	void columnNames(StringBuilder sb);
-	void writeStandardContestant();
+	void printResult(DataStructure ds, PrintWriter pw, StringBuilder sb);
 	
 
 }

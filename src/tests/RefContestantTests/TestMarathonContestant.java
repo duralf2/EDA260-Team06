d7980f65e1535157e3f95b012600d0a3defcd583
zionzept <dat13tha@student.lu.se>
2015-02-09 15:27:19 +0100

    Added compareTo() to MarathonContestant. Hannah & Kevin

--- src/tests/RefContestantTests/TestMarathonContestant.java
+++ src/tests/RefContestantTests/TestMarathonContestant.java
@@ -39,5 +39,20 @@ public class TestMarathonContestant {
 		String match = "Hannah;00.01.14;12.00.01;12.01.15";
 		assertEquals(match, racer.toString());
 	}
+	
+	@Test
+	public void testCompareTo(){
+		RacerInfo racerInfo2 = new RacerInfo();
+		racerInfo2.put("Name", "Kevin");
+		MarathonContestant racerAwesome = new MarathonContestant(racerInfo2);
+		
+		racer.addStartTime(new Time("12.00.01"));
+		racer.addFinishTime(new Time("12.02.15"));
+		
+		racerAwesome.addStartTime(new Time("12.00.01"));
+		racerAwesome.addFinishTime(new Time("12.01.15"));
+		
+		assertEquals(-1, racer.compareTo(racerAwesome) );
+	}
 
 }

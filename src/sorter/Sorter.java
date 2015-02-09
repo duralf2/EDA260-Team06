f49e43ecad4337b9e0c2e5ecbbc256f9c501f6bd
Gustaf Oxenstierna <his13gox@student.lu.se>
2015-02-09 11:45:23 +0100

    Implementerat sortering och utskrift av resultat i egen jarfil. Story 1.1 Release. Kevin&Gustaf

--- src/sorter/Sorter.java
+++ src/sorter/Sorter.java
@@ -5,6 +5,7 @@ import io.ReadFile;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,7 +15,7 @@ import register.model.Time;
 
 /**
  * This class is responsible for loading the data collected during the contests
- *  and compile it into sorted result files.
+ * and compile it into sorted result files.
  */
 public class Sorter {
 	DataStructure ds;
@@ -24,55 +25,69 @@ public class Sorter {
 	}
 
 	/**
-	 * Reads and sorts the collected data. After the data is sorted it is printed
-	 *  to a results file.  
-	 * @param files All the files containing finish times
-	 * @throws IOException If any of the files doesn't exist or couldn't be closed
+	 * Reads and sorts the collected data. After the data is sorted it is
+	 * printed to a results file.
+	 * 
+	 * @param files
+	 *            All the files containing finish times
+	 * @param nameFile
+	 *            TODO
+	 * @throws IOException
+	 *             If any of the files doesn't exist or couldn't be closed
 	 */
-	public void sortTime(File[] files) throws IOException {
+	public void sortTime(File[] files, File nameFile) throws IOException {
 		TreeMap<Time, Contestant> results = new TreeMap<Time, Contestant>();
-		
+
 		ds.clearContestantEntries();
-		
-		ReadFile.readNames(new File("testfiles/namn.txt"), ds);
+		if (nameFile == null)
+			ReadFile.readNames(new File("testfiles/namn.txt"), ds);
+		else
+			ReadFile.readNames(nameFile, ds);
 		ReadFile.readStartTime(files[0], ds);
 		ReadFile.readFinishTime(files[1], ds);
-		
-		Map<String,Contestant> contestants = ds.getAllContestantEntries();
-		for(String startNumber : contestants.keySet()){
+
+		Map<String, Contestant> contestants = ds.getAllContestantEntries();
+		for (String startNumber : contestants.keySet()) {
 			Contestant con = contestants.get(startNumber);
 			results.put(new Time(con.getTotalTime()), con);
 		}
-		
-		int i = 1;
-		for(Time t : results.keySet()){
-			System.out.println(i + ") " + t.toString() + " : " + results.get(t).getName());
-			i++;
-		}
-		//TODO: change this file to be a parameter for the function
-		File resultFile = new File("testfiles/results.txt");
-		writeToFile(results, resultFile);
+		// TODO: change this file to be a parameter for the function
+		File resultFile = new File("data/results.txt");
+		writeToFile(results, resultFile, nameFile);
 	}
-	
-	//private method for writing to file
-	private void writeToFile(TreeMap<Time, Contestant> result, File resultFile) {
+
+	// private method for writing to file
+	private void writeToFile(TreeMap<Time, Contestant> result, File resultFile,
+			File nameFile) {
 		try {
-			if(! resultFile.exists()){
-				resultFile.createNewFile();
-			}
+			resultFile.createNewFile();
 			PrintWriter pw = new PrintWriter(resultFile);
-			for (Time t: result.keySet()) {
-				pw.write(result.get(t).getName() + "; " + t.toString() + "\n");
+			int i = 1;
+			List<String[]> startNumbers;
+			if (nameFile != null)
+				startNumbers = ReadFile.readCSV(nameFile);
+			else
+				startNumbers = ReadFile.readCSV(new File("testfiles/namn.txt"));
+			pw.write("Placering; StartNr; Namn; Totaltid\n");
+			for (Time t : result.keySet()) {
+				String s = "";
+				for (String[] s1 : startNumbers) {
+					if (s1[1].trim().equalsIgnoreCase(result.get(t).getName()))
+						s = s1[0];
+				}
+				pw.write(i + "; " + s + "; " + result.get(t).getName() + "; "
+						+ t.toString() + "\n");
+				i++;
 			}
 			pw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-		
-		//TODO Skriv ut det i fönstret, eller i fil. 
-		// 1) clear ds
-		// 2) LADDA IN FILER
-		// 3) LÄGG IN I TREE MAP
-		// 4) SKRIV UT TREE MAP
+
+	// TODO Skriv ut det i fönstret, eller i fil.
+	// 1) clear ds
+	// 2) LADDA IN FILER
+	// 3) LÄGG IN I TREE MAP
+	// 4) SKRIV UT TREE MAP
 }

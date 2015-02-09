f20e6d5d7d7c706424e78ad209e2f46c95335ff9
Ella Eriksson <ada10eer@student.lu.se>
2015-02-09 14:52:07 +0100

    Added CompetionType interface + subclasses. Refactory story. Ella Magnus

---
+++ src/register/model/LapRace.java
@@ -0,0 +1,25 @@
+package register.model;
+
+import java.io.PrintWriter;
+
+public class LapRace implements CompetitionType {
+
+	@Override
+	public void result(DataStructure ds, PrintWriter pw) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void columnNames(StringBuilder sb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void writeStandardContestant() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

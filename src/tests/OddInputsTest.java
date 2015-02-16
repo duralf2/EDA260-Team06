369c8c96e8fcb524458bad8df08e6ea8db002c9f
hannushka <lindbladhannah@gmail.com>
2015-02-16 15:54:57 +0100

    Fixed OddInputTest with Filewriter
    hannah och niklas

--- src/tests/OddInputsTest.java
+++ src/tests/OddInputsTest.java
@@ -6,6 +6,7 @@ import io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import register.model.AbstractContestant;
+import register.model.Configuration;
 import register.model.ContestantProperties;
 import register.model.Database;
 import register.model.MarathonCompetition;
@@ -22,94 +24,103 @@ import register.model.Time;
 
 public class OddInputsTest {
 
-	private PrintWriter pw;
+	private FileWriter fileWriter;
 	private Database db;
 	private File f;
 	private Scanner sc;
 	private AbstractContestant contestant;
+	private Configuration conf;
 
 	@Before
-	public void setUp() throws FileNotFoundException {
-		f = new File("output.txt");
-		FileOutputStream fos = new FileOutputStream(f);
-		pw = new PrintWriter(fos);
+	public void setUp() throws IOException {
+		conf = new Configuration(new File(
+				"testfiles/config/marathonContestant.ini"));
+		AbstractContestant.setConfiguration(conf);
+
+		f = new File("testfiles/outputOddInputsTest.txt");
+		fileWriter = new FileWriter(f);
 		db = new Database();
-		sc = new Scanner(f);
-		contestant = new MarathonContestant(new ContestantProperties(new String[]{"StartNr", "Namn"}));
+		contestant = new MarathonContestant(new ContestantProperties(
+				new String[] { "StartNr", "Namn" }));
 		contestant.putInformation("Namn", "Göran");
 		contestant.putInformation("StartNr", "1");
 	}
 
 	@After
-	public void tearDown() {
+	public void tearDown() throws IOException {
+		AbstractContestant.setConfiguration(new Configuration());
 		f.delete();
 	}
 
-	// TODO implement further when Filewriter is done
-	
 	@Test
-	public void testNoStartTime() throws FileNotFoundException {
+	public void testNoStartTime() throws IOException {
 		db.addContestantEntry("1", contestant);
 
 		contestant.addFinishTime(new Time("13.23.34"));
-		//FileWriter.writeResult(pw, db);
-		
-		//assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider",
-				//sc.nextLine());
-		assertEquals("1;Göran;--.--.--;Start?;13.23.34", contestant.toString(new MarathonCompetition(db)));
+		fileWriter.writeResults(conf, db);
+
+		sc = new Scanner(f);
+		assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider", sc.nextLine());
+		assertEquals("1;Göran;--.--.--;Start?;13.23.34", sc.nextLine());
 	}
 
 	@Test
-	public void testNoFinishTime() {
+	public void testNoFinishTime() throws IOException {
 		db.addContestantEntry("1", contestant);
 		contestant.addStartTime(new Time("13.23.34"));
-		//FileWriter.writeResult(pw, db);
-		//assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider",
-				//sc.nextLine());
-		assertEquals("1;Göran;--.--.--;13.23.34;Slut?", contestant.toString(new MarathonCompetition(db)));
+
+		fileWriter.writeResults(conf, db);
+
+		sc = new Scanner(f);
+		assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider", sc.nextLine());
+		assertEquals("1;Göran;--.--.--;13.23.34;Slut?", sc.nextLine());
 	}
 
-@Test
-	public void testTooFast() {
+	@Test
+	public void testTooFast() throws IOException {
 		db.addContestantEntry("1", contestant);
-		
+
 		contestant.addStartTime(new Time("13.23.34"));
 		contestant.addFinishTime(new Time("13.30.34"));
-		//FileWriter.writeResult(pw, db);
-		//assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider",
-				//sc.nextLine());
-		assertEquals(
-				"1;Göran;00.07.00;13.23.34;13.30.34;Omöjlig totaltid?",
-				contestant.toString(new MarathonCompetition(db)));
+		fileWriter.writeResults(conf, db);
+
+		sc = new Scanner(f);
+		assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider", sc.nextLine());
+		assertEquals("1;Göran;00.07.00;13.23.34;13.30.34;Omöjlig totaltid?",
+				sc.nextLine());
 	}
 
 	@Test
-	public void testMultipleStart() {
+	public void testMultipleStart() throws IOException {
 		contestant.addStartTime(new Time("13.23.34"));
 		contestant.addStartTime(new Time("13.24.35"));
 		contestant.addFinishTime(new Time("14.30.34"));
 		db.addContestantEntry("1", contestant);
-		
-		//FileWriter.writeResult(pw, db);
-		//assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider",
-		//		sc.nextLine());
+
+		fileWriter.writeResults(conf, db);
+
+		sc = new Scanner(f);
+		assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider", sc.nextLine());
 		assertEquals(
 				"1;Göran;01.07.00;13.23.34;14.30.34;Flera starttider? 13.24.35",
-				contestant.toString(new MarathonCompetition(db)));
+				sc.nextLine());
 	}
 
 	@Test
-	public void testMultipleFinish() {
+	public void testMultipleFinish() throws IOException {
 		contestant.addStartTime(new Time("13.23.34"));
 		contestant.addFinishTime(new Time("14.30.34"));
 		contestant.addFinishTime(new Time("14.31.34"));
 		db.addContestantEntry("1", contestant);
-		//FileWriter.writeResult(pw, db);
-		//assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider",
-			//	sc.nextLine());
+		
+		fileWriter.writeResults(conf, db);
+
+		sc = new Scanner(f);
+		assertEquals("StartNr;Namn;TotalTid;Starttider;Måltider", sc.nextLine());
+		
 		assertEquals(
 				"1;Göran;01.07.00;13.23.34;14.30.34;Flera måltider? 14.31.34",
-				contestant.toString(new MarathonCompetition(db)));
+				sc.nextLine());
 	}
 
 }

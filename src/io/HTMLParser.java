59f113e948ed88f1c1aecd766e406c824c1dd99e
Therese Magnusson <dat13tma@student.lu.se>
2015-02-23 16:56:08 +0100

    Worked on HTMLParser, it should now work Sebastian, Therese and Niklas Story 28/29

--- src/io/HTMLParser.java
+++ src/io/HTMLParser.java
@@ -5,9 +5,50 @@ import java.io.IOException;
 import java.util.List;
 
 public class HTMLParser {
+	
+	public static String resultToHTML(String csvInput) {
 
-	public static void resultToHTML(File f) throws IOException {
 		StringBuilder sb = new StringBuilder();
-		List<String[]> read = io.CSVReader.read(f);
+		String[] lines = csvInput.split("\n");
+		
+		sb.append("<style> table #header{ font-weight: bold; }</style>\n");
+		sb.append("<html> <body> <table border=\"1\">\n");
+		
+		for (String line : lines)
+			sb.append(convertLine(line) + "\n");
+
+		sb.append("</table> </body> </html>");
+		
+		return sb.toString().replaceAll("<table border=\"1\">\n</table>", "");
+	}
+	
+	private static String convertLine(String line) {
+
+		StringBuilder sb = new StringBuilder();
+		String[] columns = line.split(";");
+		
+		if (columns.length > 1)
+		{
+			sb.append("<tr>");
+			for (String column : columns)
+			{
+				sb.append("<td>");
+				sb.append(column);
+				sb.append("</td>");
+			}
+			sb.append("</tr>");
+		}
+		else
+		{
+			sb.append("</table>");
+			
+			sb.append("<h3>");
+			sb.append(line);
+			sb.append("</h3>");
+			
+			sb.append("<table border=\"1\">");
+		}
+		
+		return sb.toString();
 	}
 }

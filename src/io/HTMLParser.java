0442cfc8597698286b5390bac5f3b31286309a89
Sebastian-0 <dat13shj@student.lu.se>
2015-03-02 09:16:02 +0100

    Finished the amazing HTMLParser Added tests as well Niklas and Sebastian

--- src/io/HTMLParser.java
+++ src/io/HTMLParser.java
@@ -1,38 +1,49 @@
 package io;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.plaf.SliderUI;
 
 public class HTMLParser {
-	private static final Map<String, String> headerMap;
-	static
+	private final Map<String, String> headerMap;
+	public HTMLParser()
 	{
 		headerMap = new HashMap<String, String>();
-		headerMap.put("StartNo", "Start Number");
+		headerMap.put("StartNr", "Start Number");
 	}
 
-	public static String resultToHTML(String csvInput) {
+	public String resultToHTML(String csvInput) {
 
 		StringBuilder sb = new StringBuilder();
-		List<String> lines = Arrays.asList(csvInput.split("\n"));
+		List<String> lines = new ArrayList<String>(Arrays.asList(csvInput.split("\n")));
 		
 		sb.append("<style> table #header{ font-weight: bold; }</style>\n");
 		sb.append("<html> <body> <table border=\"1\">\n");
 
-		sb.append(convertLine(lines.remove(0), true));
-		for (String line : lines)
-			sb.append(convertLine(line, false) + "\n");
+		boolean wasLastLineClass = true;
+		for (String line : lines) {
+			String[] columns = line.split(";",-1);
+			
+			sb.append(convertLine(line, columns, wasLastLineClass) + "\n");
+			if (columns.length == 1) {
+				wasLastLineClass = true;
+			} else {
+				wasLastLineClass = false;
+			}
+		}
 
 		sb.append("</table> </body> </html>");
 		
 		return sb.toString().replaceAll("<table border=\"1\">\n</table>", "");
 	}
 	
-	private static String convertLine(String line, boolean header) {
+	private String convertLine(String line, String[] columns, boolean header) {
 
 		StringBuilder sb = new StringBuilder();
-		String[] columns = line.split(";");
 		
 		if (columns.length > 1)
 		{
@@ -41,7 +52,7 @@ public class HTMLParser {
 			{
 				sb.append(header ? "<th>" : "<td>");
 				sb.append(header && headerMap.containsKey(column) ? headerMap.get(column) : column);
-				sb.append(header ? "<th>" : "</td>");
+				sb.append(header ? "</th>" : "</td>");
 			}
 			sb.append("</tr>");
 		}
---
+++ src/tests/HTMLParserTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import static org.junit.Assert.*;
+import io.HTMLParser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HTMLParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testParsing() {
+		HTMLParser parser = new HTMLParser();
+		
+		String actual = parser.resultToHTML("Klassnamn\n"
+				+ "StartNr;Namn; Klubb; Godissort\n"
+				+ "0; Bertil;LTH; Lakritskola\n"
+				+ "2; Karl;;\n"
+				+ "5; StartNr; asd;fff\n"
+				+ "Klass2\n"
+				+ "Klass3\n"
+				+ "StartNr;Namn; Klubb\n"
+				+ "3; Kaos; KTH\n"
+				+ "4;; BTH");
+		
+		String expected = "<style> table #header{ font-weight: bold; }</style>\n" +
+							"<html> <body> <h3>Klassnamn</h3><table border=\"1\">\n" +
+							"<tr><th>Start Number</th><th>Namn</th><th> Klubb</th><th> Godissort</th></tr>\n" +
+							"<tr><td>0</td><td> Bertil</td><td>LTH</td><td> Lakritskola</td></tr>\n" +
+							"<tr><td>2</td><td> Karl</td><td></td><td></td></tr>\n" + 
+							"<tr><td>5</td><td> StartNr</td><td> asd</td><td>fff</td></tr>\n" +
+							"</table><h3>Klass2</h3><h3>Klass3</h3><table border=\"1\">\n" +
+							"<tr><th>Start Number</th><th>Namn</th><th> Klubb</th></tr>\n" +
+							"<tr><td>3</td><td> Kaos</td><td> KTH</td></tr>\n" +
+							"<tr><td>4</td><td></td><td> BTH</td></tr>\n" +
+							"</table> </body> </html>";
+		
+		assertEquals(expected, actual);
+	}
+
+}

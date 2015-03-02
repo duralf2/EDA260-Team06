4654f073fc7e7f0b3a9293d9dd02d7f1dcd27f4b
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-03-02 08:03:23 +0100

    Added proper table headers, started on header lookup in hashmap.

--- src/io/HTMLParser.java
+++ src/io/HTMLParser.java
@@ -2,27 +2,34 @@ package io;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 public class HTMLParser {
-	
+	private static final Map<String, String> headerMap;
+	static
+	{
+		headerMap = new HashMap<String, String>();
+		headerMap.put("StartNo", "Start Number");
+	}
+
 	public static String resultToHTML(String csvInput) {
 
 		StringBuilder sb = new StringBuilder();
-		String[] lines = csvInput.split("\n");
+		List<String> lines = Arrays.asList(csvInput.split("\n"));
 		
 		sb.append("<style> table #header{ font-weight: bold; }</style>\n");
 		sb.append("<html> <body> <table border=\"1\">\n");
-		
+
+		sb.append(convertLine(lines.remove(0), true));
 		for (String line : lines)
-			sb.append(convertLine(line) + "\n");
+			sb.append(convertLine(line, false) + "\n");
 
 		sb.append("</table> </body> </html>");
 		
 		return sb.toString().replaceAll("<table border=\"1\">\n</table>", "");
 	}
 	
-	private static String convertLine(String line) {
+	private static String convertLine(String line, boolean header) {
 
 		StringBuilder sb = new StringBuilder();
 		String[] columns = line.split(";");
@@ -32,9 +39,9 @@ public class HTMLParser {
 			sb.append("<tr>");
 			for (String column : columns)
 			{
-				sb.append("<td>");
-				sb.append(column);
-				sb.append("</td>");
+				sb.append(header ? "<th>" : "<td>");
+				sb.append(header && headerMap.containsKey(column) ? headerMap.get(column) : column);
+				sb.append(header ? "<th>" : "</td>");
 			}
 			sb.append("</tr>");
 		}
